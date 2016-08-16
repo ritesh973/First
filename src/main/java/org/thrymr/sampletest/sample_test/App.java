3face0b77692cdf644728edd3134002288d49e9b
Ritesh Kumar Singh <ritesh.kumar@thrymr.net>
2016-08-16 19:44:54 +0530

    First commit.

---
+++ src/main/java/org/thrymr/sampletest/sample_test/App.java
@@ -0,0 +1,13 @@
+package org.thrymr.sampletest.sample_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
